5e89cc4a370f12a918aab7d2340d7d9fbd1f9eca
felipemartins8058 <felipecosmemartins10@gmail.com>
2022-07-27 20:38:19 -0300

    Fixing padding error

--- src/components/ServiceLayout/styles.js
+++ src/components/ServiceLayout/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const servicesLayout = styled.section`
     position: relative;
-    padding: 10rem;
+    padding: 10rem 1rem 1rem;
 
     @media (min-width: 1200px){
         max-width: 1200px;
