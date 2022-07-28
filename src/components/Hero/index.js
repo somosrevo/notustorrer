9dbc7ef5660d4e1d6ffc6079bfa576f043acc1e1
felipemartins8058 <felipecosmemartins10@gmail.com>
2022-07-28 11:38:09 -0300

    Updating hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -8,8 +8,8 @@ export default function Hero(){
     return(
         <s.heroWrapper>
             <s.imgBackground>
-                <StaticImage src='../../images/hero.jpg'/>
-                
+                {/* <StaticImage src='../../images/hero.jpg'/> */}
+                <StaticImage src='../../images/hero2.jpg'/>
                 <s.heroTitle>
                     <t.h1 textAlign={"center"} color={"#fff"} >Fornecimento<br/>de Estações Anemométricas</t.h1>
                 </s.heroTitle>
